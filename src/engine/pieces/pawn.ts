f63ab30a8a6759ae1691cc1853c897ee6acf2c07
Sam Cappleman-Lynes <Sam.Cappleman-Lynes@softwire.com>
2017-10-06 13:40:54 +0100

    Add a test for pawns moving two spaces initially

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -4,19 +4,24 @@ import Board from '../board';
 import Square from '../square';
 
 export default class Pawn extends Piece {
+
+    private ONEJUMP: Number = 1;
+    private TWOJUMP: Number = 2;
+    private EXPONENTIAL: Number;
+
     public constructor(player: Player) {
         super(player);
+        if (player == Player.WHITE) {
+            this.EXPONENTIAL = 1;
+        } else {
+           this.EXPONENTIAL = -1; 
+        }
     }
 
     public getAvailableMoves(board: Board) {
         const position : Square = board.findPiece(this);
         const result : Array<Square> = new Array;
 
-        if (this.player == Player.WHITE)
-            result.push(Square.at(position.row + 1, position.col))
-        else
-            result.push(Square.at(position.row - 1, position.col))
-
-        return result;
+        
     }
 }
