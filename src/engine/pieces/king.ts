c290eb3d67e145a20f0dfc83d3f666016cd991b2
Bogdan Svaicovschi <bogdan.svaicovschi@softwire.com>
2026-07-10 16:57:13 +0300

    added king movement

--- src/engine/pieces/king.ts
+++ src/engine/pieces/king.ts
@@ -1,13 +1,42 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from '../square';
 
 export default class King extends Piece {
     public constructor(player: Player) {
         super(player);
     }
 
+    private checkIfLegal(row: number, col: number) {
+        if (row < 0 || row > 7 || col < 0 || col > 7) {
+            return false;
+        }
+
+        return true;
+    }
+
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const position: Square = board.findPiece(this);
+        const result: Array<Square> = new Array;
+        const col: number = position.col - 1;
+        const row: number = position.row - 1;
+
+
+        for (let i = 0; i < 3; i++) {
+            const newRow = row + i;
+            for (let j = 0; j < 3; j++) {
+                const newCol = col + j;
+                if (this.checkIfLegal(newRow, newCol)) {
+                    const pos1:Square = Square.at(newRow, newCol);
+                    if (!board.getPiece(pos1)) {
+                        result.push(pos1);
+                    }
+                }
+            }
+        }
+
+        return result;
+
     }
 }
